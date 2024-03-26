86b4cb16b85ed47d24a4105652d5478cae38ba04
Borisov Ilya Denisovich <borisov.i@ulstu.ru>
2024-03-26 21:53:52 +0400

    0.7

---
+++ AISD/AISD_BP_2.py
@@ -0,0 +1,24 @@
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+d = list(map(int, input().split()))
+
+total = 0
+
+for i in d:
+    l, r = 0, n-1
+    result = -1
+
+    while l <= r:
+        c = l + (r - l) // 2
+
+        if a[c] == i:
+            result = c
+            l = c + 1
+        elif a[c] < i:
+            l = c + 1
+        else:
+            r = c - 1
+    total += result
+
+print(total)
