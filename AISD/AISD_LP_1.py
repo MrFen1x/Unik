71d05ae4b7d780db626758749de592d793a4596f
Илья <iammasterspubg@gmail.com>
2024-03-29 19:27:56 +0400

    0.9

--- AISD/AISD_LP_1.py
+++ AISD/AISD_LP_1.py
@@ -1,14 +1,14 @@
-f= open('INPUT.TXT', 'r')
+f=open('INPUT.TXT', 'r')
 n = int(f.readline())
-a= list(map(int, f.readline().split()))
+a = list(map(int, f.readline().split()))
 l, r = map(int, f.readline().split())
 
 max_element = a[l-1]
 max_index = l
-for i in range(l,r):
+for i in range(l, r):
     if a[i] > max_element:
         max_element = a[i]
         max_index = i + 1
 
-print(max_element,max_index)
-
+f=open('OUTPUT.TXT', 'w')
+f.write(f"{max_element} {max_index}")
